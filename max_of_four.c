daaa39c6f7754d6838199e915ecca6517153bd57
manigandan-d <manigandan220916@gmail.com>
2025-08-30 08:46:27 +0530

    print the greatest of four integers

---
+++ max_of_four.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c, d; 
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    int max = a; 
+
+    if(b > max) max = b; 
+    if(c > max) max = c; 
+    if(d > max) max = d; 
+
+    printf("%d\n", max);
+
+    return 0;
+}
